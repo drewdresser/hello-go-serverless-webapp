6764ab22e5e6ff6a83654da9daec23316a948ef6
Drew Dresser <andrewjdresser@gmail.com>
2018-02-15 14:33:30 -0500

    Adding tests

--- main.go
+++ main.go
@@ -2,47 +2,30 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// ErrNameNotProvided is thrown when a name is not provided
-	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-)
-
-// var ddb *dynamodb.DynamDB
-
-// func init() {
-// 	region := os.Getenv("AWS_REGION")
-// 	if session, err := session.NewSession(&aws.Config{ // Use aws sdk to connect to dynamoDB
-// 		Region: &region,
-// 	}); err != nil {
-// 		fmt.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
-// 	} else {
-// 		ddb = dynamodb.New(session) // Create DynamoDB client
-// 	}
-// }
-
-func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Printf("Body size = %d. \n", len(request.Body))
 	fmt.Println("Headers:")
 	for key, value := range request.Headers {
 		fmt.Printf("  %s: %s\n", key, value)
 	}
 	if request.HTTPMethod == "GET" {
-		fmt.Printf("GET METHOD")
+		fmt.Printf("GET METHOD\n")
+		return events.APIGatewayProxyResponse{Body: "GET", StatusCode: 200}, nil
 	} else if request.HTTPMethod == "POST" {
-		fmt.Printf("POST METHOD")
+		fmt.Printf("POST METHOD\n")
+		return events.APIGatewayProxyResponse{Body: "POST", StatusCode: 200}, nil
 	} else {
-		fmt.Printf("NEITHER!")
+		fmt.Printf("NEITHER\n")
+		return events.APIGatewayProxyResponse{Body: "NEITHER", StatusCode: 200}, nil
 	}
-	return events.APIGatewayProxyResponse{Body: "Success!", StatusCode: 200}, nil
 }
 
 func main() {
-	lambda.Start(handleRequest)
+	lambda.Start(HandleRequest)
 }
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main_test
+
+import (
+	"testing"
+
+	main "github.com/drewdresser/hello-go-serverless-webapp"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		expect  string
+		err     error
+	}{
+		{
+			// Test that the handler responds with the correct response
+			// when a valid name is provided in the HTTP body
+			request: events.APIGatewayProxyRequest{HTTPMethod: "GET"},
+			expect:  "GET",
+			err:     nil,
+		},
+		{
+			// Test that the handler responds ErrNameNotProvided
+			// when no name is provided in the HTTP body
+			request: events.APIGatewayProxyRequest{Body: "PUT"},
+			expect:  "NEITHER",
+			err:     nil,
+		},
+	}
+
+	for _, test := range tests {
+		response, err := main.HandleRequest(nil, test.request)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.expect, response.Body)
+	}
+
+}
